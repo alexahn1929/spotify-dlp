2dbe6d644134291bbc275d15ce6c73494ae17eca
alexahn1929 <121832249+alexahn1929@users.noreply.github.com>
2023-06-21 17:43:26 -0400

    Convert Spotify API calls from node HTTPS to fetch
    Since preload scripts in Electron cannot use the node HTTPS module,
    convert Spotify API requests to use the built-in fetch API.

--- api.js
+++ api.js
@@ -1,36 +1,23 @@
 require('dotenv').config();
-const fs = require("node:fs");
 const url = require("node:url");
-const https = require("node:https");
 
 let savedToken = "PLACEHOLDER";
 
-const getToken = () => new Promise((resolve, reject) => { //to be called using the resolve/reject static functions in the promise class
+const getToken = async () => {
     const options = {
-        hostname: "accounts.spotify.com",
         method: "POST",
-        path: `/api/token?grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
         }
     };
-    
-    let responseData = [];
 
-    const req = https.request(options, (res) => {
-        if (res.statusCode != 200) {
-            reject(new Error("Could not obtain new access token"));
-        }
-        res.on('data', (d) => {responseData.push(d.toString())});
-    });
-    
-    req.once("close", () => {
-        console.log("token: "+JSON.parse(responseData.join("")).access_token)
-        resolve(JSON.parse(responseData.join("")).access_token);
-    });
-
-    req.end();
-});
+    let res = await fetch(`https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`, options);
+    if (res.status != 200) {
+        throw new Error("Could not obtain new access token");
+    }
+    let output = await res.json();
+    return output.access_token;
+};
 
 const getPlaylistAPIUrl = (link) => {
     const HOSTNAME = "open.spotify.com";
@@ -46,35 +33,35 @@ const getPlaylistAPIUrl = (link) => {
     return `https://api.spotify.com/v1/playlists/${playlistID}/tracks`;
 };
 
-const getPlaylist = (apiURL) => new Promise((resolve, reject) => {
+const getPlaylist = async (apiURL) => {
     const options = {
         headers: {"Authorization": `Bearer ${savedToken}`}
     };
-    let responseData = [];
+    let res = await fetch(apiURL, options);
+    if (res.status == 401) { //expired token
+        savedToken = await getToken();
+        return getPlaylist(apiURL);
+    } else if (res.status == 403) {
+        throw new Error("Bad OAuth request");
+    } else if (res.status == 429) {
+        throw new Error("API rate limit exceeded");
+    }
+    return res.json();
+};
 
-    const req = https.request('https://api.spotify.com/v1/playlists/1ID56tk92tPTeIJ5jH8aUb/tracks?offset=100&limit=99', options, (res) => {
-        if (res.statusCode == 401) { //expired token
-            resolve(getToken().then((newToken) => {
-                savedToken = newToken;
-                console.log("token: "+savedToken)
-                return getPlaylist(apiURL);
-            }));
-        } else if (res.statusCode == 403) {
-            reject(new Error("Bad OAuth request"));
-        } else if (res.statusCode == 429) {
-            reject(new Error("API rate limit exceeded"));
-        }
-        res.on('data', (d) => {responseData.push(d.toString())});
-    });
-    
-    req.once("close", () => {
-        const output = JSON.parse(responseData.join(""));
-        resolve(output);
-        //to do: pull jsons for entire playlist (ceil(# songs//100) API requests) and concat into single array
-    });
+const getPlaylistFromRawURL = async (rawURL) => {
+    let apiURL = getPlaylistAPIUrl(rawURL);
+    let playlistJSON = await getPlaylist(apiURL);
+    let songs = playlistJSON.items;
+    while (playlistJSON.next !== null) {
+        playlistJSON = await getPlaylist(playlistJSON.next);
+        songs = songs.concat(playlistJSON.items);
+    }
+    return songs;
+}
 
-    req.end();
-});
 
-//test();
-getPlaylist(getPlaylistAPIUrl("https://open.spotify.com/playlist/1ID56tk92tPTeIJ5jH8aUb?si=53c3f6f9a151418a")).then((output) => console.log(output));
+getPlaylistFromRawURL("https://open.spotify.com/playlist/3AFvuS9t4qLhaaLBHRcSqk?si=0b8e8e8e9d1447ad").then(c => {
+    console.log(c)
+    console.log(c.length)
+})
